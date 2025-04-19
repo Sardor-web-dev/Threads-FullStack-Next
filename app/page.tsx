1f4168d12c5602a10dc250cd93f33c93fb97e231
SarikViper <dzamolovsardor5@gmail.com>
2025-04-19 22:22:43 +0500

    -

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Post from "@/components/custom/Post";
 
 export default function Home() {
-	return (
-		<div className="">
-			<div className="flex flex-col p-10 gap-5 items-start">
-				<Post />
-			</div>
-		</div>
-	);
+  return (
+    <div className="">
+      <div className="flex overflow-y-auto flex-col p-10 gap-5 items-start">
+        <Post />
+      </div>
+    </div>
+  );
 }
