95a0bded7f3ff5b91e21579dda76ed7d9e4415ec
SarikViper <dzamolovsardor5@gmail.com>
2025-04-20 22:14:48 +0500

    -

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Post from "@/components/custom/Post";
 import { Thread } from "@/types/thread";
 
 export default async function Home() {
-  const response: Thread[] = await fetch("http://localhost:3000/api/data").then(
+  const response: Thread[] = await fetch("https:/threads-uz.netlify.app//api/data").then(
     (res) => res.json()
   );
   return (
