60bf57dc5635007824eaa47a46b2388f4521b458
SarikViper <dzamolovsardor5@gmail.com>
2025-04-20 22:35:57 +0500

    created server and fixed bugs for deploy

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const page: React.FC = () => {
-	return <div></div>;
-  };
-  
-  export default page;
-  
+  return (
+    <div className="flex flex-col gap-5 items-center justify-center mt-10">
+      <Input className="w-50" placeholder="Найдите пользователей" />
+      <Button className="bg-gray-500 hover:bg-white hover:text-black cursor-pointer text-white transition-all">Search</Button>
+    </div>
+  );
+};
+
+export default page;
