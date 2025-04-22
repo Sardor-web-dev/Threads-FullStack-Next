e5cf0d08e2fb968f112c38d053094edac37d89cc
SarikViper <dzamolovsardor5@gmail.com>
2025-04-23 01:40:11 +0500

    fixed all bugs and uploaded

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,22 @@
 import NextAuth from "next-auth";
-// import GithubProvider from "next-auth/providers/github";
-// import InstagramProvider from "next-auth/providers/instagram";
-// import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import InstagramProvider from "next-auth/providers/instagram";
+import GoogleProvider from "next-auth/providers/google";
 
 const authOptions = {
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID ?? "",
-    //   clientSecret: process.env.GITHUB_SECRET ?? "",
-    // }),
-    // InstagramProvider({
-    //   clientId: process.env.INSTAGRAM_ID ?? "",
-    //   clientSecret: process.env.INSTAGRAM_SECRET ?? "",
-    // }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_ID ?? "",
-    //   clientSecret: process.env.GOOGLE_SECRET ?? "",
-    // }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    InstagramProvider({
+      clientId: process.env.INSTAGRAM_ID ?? "",
+      clientSecret: process.env.INSTAGRAM_SECRET ?? "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
   ],
 };
 
