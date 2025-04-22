77f313c5b8c2a7728bfef627fda615c8e999fa95
SarikViper <dzamolovsardor5@gmail.com>
2025-04-23 01:00:38 +0500

    -

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Post from "@/components/custom/Post";
-import { Thread } from "@/types/thread";
+// import Post from "@/components/custom/Post";
+// import { Thread } from "@/types/thread";
 
 export default async function Home() {
-  const response: Thread[] = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/data`).then(
-    (res) => res.json()
-  );
+  // const response: Thread[] = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/data`).then(
+  //   (res) => res.json()
+  // );
   return (
     <div className="">
-      <div className="flex overflow-y-auto flex-col p-10 gap-10 items-start">
+      {/* <div className="flex overflow-y-auto flex-col p-10 gap-10 items-start">
         {response.map((thread: Thread) => (
           <Post key={thread.id} thread={thread} />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
