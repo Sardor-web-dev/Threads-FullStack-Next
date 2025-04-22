0c996f2505c851e574bab543acaa50f9ced8e7ff
SarikViper <dzamolovsardor5@gmail.com>
2025-04-23 01:37:03 +0500

    a

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,22 +3,22 @@
 // import InstagramProvider from "next-auth/providers/instagram";
 // import GoogleProvider from "next-auth/providers/google";
 
-// const authOptions = {
-//   providers: [
-//   //   GithubProvider({
-//   //     clientId: process.env.GITHUB_ID ?? "",
-//   //     clientSecret: process.env.GITHUB_SECRET ?? "",
-//   //   }),
-//   //   InstagramProvider({
-//   //     clientId: process.env.INSTAGRAM_ID ?? "",
-//   //     clientSecret: process.env.INSTAGRAM_SECRET ?? "",
-//   //   }),
-//   //   GoogleProvider({
-//   //     clientId: process.env.GOOGLE_ID ?? "",
-//   //     clientSecret: process.env.GOOGLE_SECRET ?? "",
-//   //   }),
-//   // ],
-// };
+const authOptions = {
+  providers: [
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID ?? "",
+    //   clientSecret: process.env.GITHUB_SECRET ?? "",
+    // }),
+    // InstagramProvider({
+    //   clientId: process.env.INSTAGRAM_ID ?? "",
+    //   clientSecret: process.env.INSTAGRAM_SECRET ?? "",
+    // }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID ?? "",
+    //   clientSecret: process.env.GOOGLE_SECRET ?? "",
+    // }),
+  ],
+};
 
-// const handler = NextAuth(authOptions);
-// export { handler as GET, handler as POST };
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
